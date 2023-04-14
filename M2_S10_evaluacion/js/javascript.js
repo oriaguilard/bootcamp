ea545d81cde29ce33d0a8f620b0e61e904cad805
oriaguilar <aguilar.orianna@gmail.com>
2023-04-13 20:13:43 -0400

    agregando modificaciones

--- M2_S10_evaluacion/js/javascript.js
+++ M2_S10_evaluacion/js/javascript.js
@@ -1,3 +1,17 @@
+function mostrarFormulario(formularioId) {
+    // Ocultar todos los formularios
+    document.getElementById('formulario1').style.display = 'none';
+    document.getElementById('formulario2').style.display = 'none';
+  
+    // Mostrar el formulario seleccionado
+    if (formularioId === 1) {
+      document.getElementById('formulario1').style.display = 'block';
+    } else if (formularioId === 2) {
+      document.getElementById('formulario2').style.display = 'block';
+    }
+  }
+
+
 //ejecutar evento submit
 $("#reserva").submit(function (event) {
     event.preventDefault();
@@ -29,6 +43,35 @@ $("#reserva").submit(function (event) {
         alert("El campo asistentes es obligatorio")   
     }
 
+
+    alert("Estimado " + nombre + " agradecemos por reservar con nosotros. Hemos registrado " +asistentes + " asistentes. Se ha enviado el código de confirmación al correo"  + mail + "Gracias por preferirnos.")  
+})
+
+
+//ejecutar evento submit
+$("#comuniquemonos").submit(function (event) {
+    event.preventDefault();
+
+    // valores del input
+    var nombre = $("#nombreComuniquemonos").val(); 
+    var mail = $("#correoComuniquemonos").val();
+    var telefono = $("#telefonoComuniquemonos").val();
+    var motivo = $("#disabledSelect").val();
+
+
+   //validar formulario//campos obligatorios
+    if(nombre == ""){
+        alert("El campo nombre es obligatorio")  
+    } else if (mail == "" ||  mail == null){
+        alert("El campo correo es obligatorio") 
+         
+    } else if (telefono == ""){
+        alert("El campo telefono es obligatorio")  
+         
+    } else if (motivo == ""){
+        alert("El campo motivo es obligatorio")  
+         
+    } 
 
     alert("Muchas gracias " + nombre + " hemos recibido su sugerencia y enviaremos una pronta repuesta al correo " + mail)  
 })
--- M2_S9_evaluacion/js/javascript.js
+++ M2_S9_evaluacion/js/javascript.js
@@ -1,24 +1,77 @@
+function mostrarFormulario(formularioId) {
+    // Ocultar todos los formularios
+    document.getElementById('formulario1').style.display = 'none';
+    document.getElementById('formulario2').style.display = 'none';
+  
+    // Mostrar el formulario seleccionado
+    if (formularioId === 1) {
+      document.getElementById('formulario1').style.display = 'block';
+    } else if (formularioId === 2) {
+      document.getElementById('formulario2').style.display = 'block';
+    }
+  }
 
-var formComuniquemonos = document.getElementById("comuniquemonos")
-var botonEnviar = document.getElementById("enviar")
+
+//ejecutar evento submit
+$("#reserva").submit(function (event) {
+    event.preventDefault();
+
+    // valores del input
+    var nombre = $("#nombreReserva").val(); 
+    var mail = $("#mailReserva").val();
+    var telefono = $("#telefonoReserva").val();
+    var fecha = $("#fechaReserva").val();
+    var  hora = $("#horaReserva").val();
+    var asistentes = $("#asistentesReserva").val();
+
+    //validar formulario//campos obligatorios
+    if(nombre == ""){
+        alert("El campo nombre es obligatorio")  
+    } else if (mail == "" ||  mail == null){
+        alert("El campo correo es obligatorio") 
+         
+    } else if (telefono == ""){
+        alert("El campo telefono es obligatorio")  
+         
+    } else if (fecha == ""){
+        alert("El campo fecha es obligatorio")  
+         
+    } else if (hora == ""){
+        alert("El campo hora es obligatorio")  
+         
+    } else if (asistentes == ""){
+        alert("El campo asistentes es obligatorio")   
+    }
 
 
+    alert("Muchas gracias " + nombre + " hemos recibido su reservación y enviaremos un respaldo al correo " + mail)  
+})
 
 
+//ejecutar evento submit
+$("#comuniquemonos").submit(function (event) {
+    event.preventDefault();
+
+    // valores del input
+    var nombre = $("#nombreComuniquemonos").val(); 
+    var mail = $("#correoComuniquemonos").val();
+    var telefono = $("#telefonoComuniquemonos").val();
+    var motivo = $("#disabledSelect").val();
 
 
+   //validar formulario//campos obligatorios
+    if(nombre == ""){
+        alert("El campo nombre es obligatorio")  
+    } else if (mail == "" ||  mail == null){
+        alert("El campo correo es obligatorio") 
+         
+    } else if (telefono == ""){
+        alert("El campo telefono es obligatorio")  
+         
+    } else if (motivo == ""){
+        alert("El campo motivo es obligatorio")  
+         
+    } 
 
-
-
-/*
-function formulario(comuniquemonos){
-   comuniquemonos.preventDefault();
-
-    //los id que estan en el formulario
-    var nombre = document.getElementById('nombre').value;
-    var correo = document.getElementById('correo').value;
-    var telefono = document.getElementById('telefono').value;
-    var select = document.getElementById('disabledSelect').value;
-
-    alert("Muchas gracias " + nombre + " hemos recibido su sugeencua y enviaremos una pronta repuesta al correo " + correo)  
-}*/
+    alert("Muchas gracias " + nombre + " hemos recibido su sugerencia y enviaremos una pronta repuesta al correo " + mail)  
+})
